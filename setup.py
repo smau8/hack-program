79378e7b516ef3cce66d4c859c62987af0b44dc1
smau8 <sma2244@columbia.edu>
2021-02-10 14:54:27 -0800

    add setup.py for timezone package

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Install timezone package. 
+Call `pip install -e .` to install package locally for testing.
+"""
+
+from setuptools import setup
+
+# build command
+setup(
+    name="timezone",
+    version="0.0.1",
+    description="A package for displaying US timezones",
+    classifiers=["Programming Language :: Python :: 3"],
+    entry_points={
+        "console_scripts": ["timezone = timezone.__main__:run_program"],
+    },
+)
